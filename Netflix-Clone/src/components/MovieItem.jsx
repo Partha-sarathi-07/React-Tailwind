ed2ab7111cf30537cbf2dccd70deb1760f6002a1
Partha-sarathi-07 <sarathicharu07@gmail.com>
2024-04-26 09:48:54 +0530

    half commit

--- Netflix-Clone/src/components/MovieItem.jsx
+++ Netflix-Clone/src/components/MovieItem.jsx
@@ -1,12 +1,16 @@
+import React from "react";
+import { createImageUrl } from "../services/MovieServices"
 
-const MovieItem = ({movie}) => {
+
+const MovieItem = ({ movie }) => {
+  const {title, backdrop_path, poster_path} = movie;
   return (
-    <div>
-      <p>
-        {`${movie.title} | ${movie.id}`}
-      </p>
+    <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2">
+        <img
+         className="w-full h-40 block object-cover object-top"
+        src={createImageUrl(backdrop_path ?? poster_path, "w-500")} alt={title} />
     </div>
-  )
-}
+  );
+};
 
-export default MovieItem
+export default MovieItem; 
